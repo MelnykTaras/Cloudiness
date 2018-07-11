1a77c8a9777315a066d1789472194b8d2bbf7fa5
TarasMelnyk <taras.melnyk.n@gmail.com>
2018-07-11 18:55:47 +0300

    Init project

---
+++ Cloudiness/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Cloudiness
+//
+//  Created by Admin on 7/11/18.
+//  Copyright © 2018 Taras Melnyk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Cloudiness/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Cloudiness
+//
+//  Created by Admin on 7/11/18.
+//  Copyright © 2018 Taras Melnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
