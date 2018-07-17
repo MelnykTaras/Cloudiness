6071a9fe1d492cbe59ec55f9648cbc6667d88ed7
TarasMelnyk <taras.melnyk.n@gmail.com>
2018-07-17 17:14:46 +0300

    Refactoring UIAlertControllerExtension: make method private, reorder

--- Cloudiness/Extensions/UIAlertControllerExtension.swift
+++ Cloudiness/Extensions/UIAlertControllerExtension.swift
@@ -10,14 +10,14 @@ import UIKit
 
 extension UIAlertController {
     
-    func shouldEnableSaveButton(_ locationTitle: String) -> Bool {
-        return !locationTitle.isEmpty
-    }
-    
     @objc func textDidChangeInTextField() {
         if let locationTitle = textFields?[0].text,
            let action = actions.last {
             action.isEnabled = shouldEnableSaveButton(locationTitle)
         }
+    }
+    
+    private func shouldEnableSaveButton(_ locationTitle: String) -> Bool {
+        return !locationTitle.isEmpty
     }
 }
